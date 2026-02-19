4559a856e7787274a7335e1137d13785097475d8
rudranhit-tech <rudra.nhit@gmail.com>
2026-02-19 11:57:48 +0000

    practice

--- 01_basics/01_Varriables.js
+++ 01_basics/01_Varriables.js
@@ -1,1 +1,11 @@
-console.log("Rudra")
+const accountId = 456879;
+let accountEmail = "rudra@gmail.com";
+var accountName = "Rudra";
+accountCity = "Jaipur";
+
+// console.log(accountId);
+accountCity = "Durgapur";
+accountName = "Suman";
+accountEmail = "rc@rc.com"
+
+console.table ([accountEmail, accountId, accountName, accountCity]);
